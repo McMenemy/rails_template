7f95e6c3ef0c75bc166d200059ccf7f37eff7e29
jabberwick <mr_mcmenemy@yahoo.com>
2015-06-01 23:16:31 +0000

    testing sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => ENV['app37289598@heroku.com'],
+    :password       => ENV['auvfk8765626'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
